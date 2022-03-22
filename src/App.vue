91eff2843af1874364c22bc1c3cb99bd2dbb025b
felipFG   ÃGómez <felipe.lfg95@gmail.com>
2022-03-22 11:32:38 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>app container</h1>
+  <Register />
+</template>
+
+<script>
+import Register from "./components/register/register.vue";
+
+export default {
+  components: { Register },
+};
+</script>
---
+++ src/components/register/register.vue
@@ -0,0 +1,43 @@
+<template>
+  <form action="" @submit="handleRegister($event)">
+    <div>
+      <input v-model="model.name" type="text" placeholder="nombre" />
+    </div>
+    <div>
+      <select v-model="model.typeId" id="">
+        <option value="opcion1">opcion 1</option>
+        <option value="opcion2">opcion 2</option>
+      </select>
+      <input
+        v-model="model.numberId"
+        type="text"
+        placeholder="numero de documento"
+      />
+    </div>
+    <div>
+      <button type="submit">enviar</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      model: {
+        name: "",
+        typeId: "",
+        numberId: "",
+      },
+    };
+  },
+  methods: {
+    handleRegister(event) {
+      event.preventDefault();
+      console.log(this.model.name);
+      console.log(this.model.typeId);
+      console.log(this.model.numberId);
+    },
+  },
+};
+</script>
